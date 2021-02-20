6bcb86950d54873567723f7346e2f62577b154f9
Rick Spencer <richard.linger.spencer.3@gmail.com>
2021-02-20 16:01:30 -0500

    renamed some functions and added pydocs

--- py2fluxcsv.py
+++ py2fluxcsv.py
@@ -2,6 +2,7 @@ import os
 from io import StringIO
 
 def convert(input_list):
+    """Returns annotated csv for a list of lists of dictionaries."""
     all_tables = ""
     for i, l in enumerate(input_list):
         if (i > 0):
@@ -10,13 +11,16 @@ def convert(input_list):
     return all_tables
 
 def list_to_csv(i, dictionaries):
+    """Converts a list of dictonaries to annotated CSV."""
     keys = keys_for_table(dictionaries)
-    lines = header_for_table(keys)
+    lines = annotation_for_table(keys)
     for d in dictionaries:
-        lines += line_from_dict(i, keys, d)
+        lines += csv_for_dict(i, keys, d)
     return lines
 
-def header_for_table(keys):
+def annotation_for_table(keys):
+    """Creates the annotation for the keys of a dictionary.
+    Used to create the annotation for a csv."""
     header = ",result,table"
     for key in keys:
         header += ",{}".format(key)
@@ -24,9 +28,11 @@ def header_for_table(keys):
     return header
 
 def keys_for_table(dictionaries):
+    """Gets the keys for a given list of dictionaries."""
     return dictionaries[0].keys()
 
-def line_from_dict(i, keys, d):
+def csv_for_dict(i, keys, d):
+    """Gets the csv for a specific dictionary."""
     line = ",_result"
     line += ",{}".format(i)
     for key in keys:
@@ -34,17 +40,11 @@ def line_from_dict(i, keys, d):
     line += os.linesep
     return line
 
-def write_to_buff(buff, input_list):
-    for i, l in enumerate(input_list):
-        keys = keys_for_table(l)
-        buff.write(header_for_table(keys))
-        if(i > 0):
-            buff.write(os.linesep)
-        buff.flush()
-        for dictionary in l:
-            buff.write(line_from_dict(i, keys, dictionary))
-            buff.flush()
-
+"""An iterable class that returns annotated CSV one line at a time.
+Designed for scenarios where it is undesirable to convert an entire list of
+lists of dictionaries into one entire string. For example, when streaming
+from an http server.
+"""
 class CSVIter:
     def __init__(self, input_list):
         self._input_list = input_list
@@ -65,10 +65,10 @@ class CSVIter:
             self._current_keys = keys_for_table(self._input_list[self._table_index])
             if(self._table_index > 0):
                 data += os.linesep
-            data += header_for_table(self._current_keys)
+            data += annotation_for_table(self._current_keys)
             
         d = self._input_list[self._table_index][self._dictionary_index]
-        data += line_from_dict(self._table_index, self._current_keys, d)
+        data += csv_for_dict(self._table_index, self._current_keys, d)
 
         self._dictionary_index += 1
         if self._dictionary_index == len(self._input_list[self._table_index]):
